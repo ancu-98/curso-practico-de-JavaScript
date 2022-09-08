4a6527f604c5a4a894e22475bf259f7b6e7ba51c
Ancu-98 <jdsebastianb30@hotmail.com>
2022-09-08 17:58:05 -0500

    Actualizacion clase 22

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 
@@ -12,22 +12,22 @@ menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     // Si Aside esta open, hay que cerrarlo
     if (!isAsideClosed) {
-        aside.classList.toggle('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
     // Si Aside esta open, hay que cerrarlo
     if (!isAsideClosed) {
-        aside.classList.toggle('inactive');
+        shoppingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -37,10 +37,10 @@ function toggleCarritoAside() {
 
     // Si Mobile Menu esta open, hay que cerrarlo
     if (!isMobileMenuClosed) {
-        mobileMenu.classList.toggle('inactive');
+        mobileMenu.classList.add('inactive');
     }
 
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 
 }
 
